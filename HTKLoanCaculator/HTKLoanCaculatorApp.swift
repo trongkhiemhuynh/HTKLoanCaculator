485b72f58073b730657e909c6158761e94d70600
Khiem Huynh <trongkhiem.huynh@gmail.com>
2021-07-06 13:59:38 +0700

    Initial Commit

---
+++ HTKLoanCaculator/HTKLoanCaculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTKLoanCaculatorApp.swift
+//  HTKLoanCaculator
+//
+//  Created by khiemht on 06/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct HTKLoanCaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
