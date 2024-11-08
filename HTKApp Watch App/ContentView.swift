44acf6610e8d5ab986098d175781e7cec911ca59
Khiem T. Huynh <htkhiem@cmcglobal.vn>
2024-11-08 15:43:42 +0700

    add checkbox view into applewatch

--- HTKApp Watch App/ContentView.swift
+++ HTKApp Watch App/ContentView.swift
@@ -8,6 +8,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello world")
+        VStack {
+            Text("My todoist").font(.headline)
+            
+            List {
+                HStack {
+                    Button(action: {print("click")}) {
+                        CheckBoxView(isChecked: false)
+                    }
+                    Text("Cafe")
+                }
+                
+                Text("Podcasts")
+                Text("Running")
+                Text("Reading")
+                
+                HStack {
+                    Button(action: {print("click")}) {
+                        CheckBoxView(isChecked: true)
+                    }
+                    Text("Coding")
+                }
+            }.listStyle(.elliptical)
+        }
     }
 }
